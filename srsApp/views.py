b98a9c61d15d2764a07d605a764e3a884fccd6fd
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2024-10-21 22:26:48 -0600

    Created login view

--- srsApp/views.py
+++ srsApp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'srsApp/home.html') 
+
+def login_view(request):
+    # add login logic
+    return render(request, 'srsApp/login.html') 

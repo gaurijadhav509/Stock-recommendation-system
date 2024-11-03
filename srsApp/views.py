47749232e833ce4ecea78817b8d67f973438af5c
Sanket Patel <sanketpatel111020@gmail.com>
2024-11-03 16:01:02 -0700

    login page connection with mysql databse done

--- srsApp/views.py
+++ srsApp/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home_view(request):
     return render(request, 'home.html') 
 
 def login_view(request):
     # add login logic
-    return render(request, 'login.html') 
+    return render(request, 'login.html')
+
+def login_user(request):
+    email_v = request.POST['u_email']
+    password_v = request.POST['u_password']
+
+    user = authenticate(request, username=email_v, password=password_v)
+        
+    if user is not None:
+        login(request, user)
+        return redirect('home')  # Redirect to a success page
+    else:
+        messages.error(request, 'Invalid username or password')
+    
+    return render(request, 'login_success.html', {})
+
+def login_success(request):
+    return render(request, 'login_success.html')
+
 
 def user_signup_view(request):
     return render(request, 'signup.html')

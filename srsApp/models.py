27e54fc83f1b677937fbc650cfb8234f8fe39a48
Sanket Patel <sanketpatel111020@gmail.com>
2024-11-12 00:53:28 -0700

    Models modification as per ER diagram and raw queries for signin,signup

--- srsApp/models.py
+++ srsApp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#### Entity Tables.
+### 1. Users Table
 class Users(models.Model):
     user_id = models.AutoField(primary_key=True, null=False)
     name = models.CharField(max_length=50, null=False)
@@ -11,7 +13,9 @@ class Users(models.Model):
 
     class Meta:
         db_table = "Users"
-    
+
+### 1. Stocks Table
+
 class Stocks(models.Model):
     stock_id = models.AutoField(primary_key=True, null=False)
     stock_symbol = models.CharField(max_length=10, null=False)
@@ -23,28 +27,7 @@ class Stocks(models.Model):
     class Meta:
         db_table = "Stocks"
 
-class Recommendation(models.Model):
-    recommendation_id = models.AutoField(primary_key=True, null=False)
-    recommended_at = models.DateTimeField(auto_now_add=True)
-    user_id = models.IntegerField(null=False)
-    stock_id = models.IntegerField(null=False)
-    # user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    # stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "Recommendation"
-    
-class Bookmarked_Stock(models.Model):
-    bookmarked_id = models.AutoField(primary_key=True, null=False)
-    bookmarked_at = models.DateTimeField(auto_now_add=True)
-    user_id = models.IntegerField(null=False)
-    stock_id = models.IntegerField(null=False)
-    # user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    # stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "Bookmarked_Stocks"
-    
+#### Investment Preferences Table.
 class Investment_Prefrences(models.Model):
     class Risk_Tolerance(models.IntegerChoices):
         LOW = 1,
@@ -55,7 +38,7 @@ class Investment_Prefrences(models.Model):
         STOcKS = 1,
         BONDS = 2,
         OPTIONS = 3,
-    prefrences_id = models.AutoField(primary_key=True, null=False)
+    prefrence_id = models.AutoField(primary_key=True, null=False)
     user_id = models.IntegerField(null=False)
     preferred_region = models.CharField(max_length=100)
     preferred_exchage = models.CharField(max_length=100)
@@ -72,48 +55,32 @@ class Investment_Prefrences(models.Model):
     class Meta:
         db_table = "Investment_Prefrences"
 
-# Tables for Relationships
-class user_bookmarked_stocks_rel:
+##### Tables for Relationships
+
+## 1. User_Boookmarked Stocks
+class user_bookmarked_stocks:
     user_id = models.IntegerField(primary_key=True, null=False)
     stock_id = models.IntegerField(primary_key=True, null=False)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "user_bookmarked_stocks_rel"
+        db_table = "user_bookmarked_stocks"
 
-class user_recommended_stocks_rel:
+class user_investment_prefrences:
     user_id = models.IntegerField(primary_key=True, null=False)
-    stock_id = models.IntegerField(primary_key=True, null=False)
+    prefrence_id = models.IntegerField(primary_key=True, null=False)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE)
+    prefrence_id = models.ForeignKey(Investment_Prefrences, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "user_recommended_stocks_rel"
+        db_table = "user_investment_prefrences"
 
-class recommendation_to_stock_rel:
-    recommendation_id = models.IntegerField(primary_key=True, null=False)
+class stock_investment_prefrences:
+    prefrence_id = models.IntegerField(primary_key=True, null=False)
     stock_id = models.IntegerField(primary_key=True, null=False)
-    recommendation_id = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
+    prefrence_id = models.ForeignKey(Investment_Prefrences, on_delete=models.CASCADE)
     stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "recommendation_to_stock_rel"
-
-class stock_to_bookmarked_stock_rel:
-    user_id = models.IntegerField(primary_key=True, null=False)
-    stock_id = models.IntegerField(primary_key=True, null=False)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "stock_to_bookmarked_stock_rel"
-
-class investment_preference_to_recommendation_rel:
-    recommendation_id = models.IntegerField(primary_key=True, null=False)
-    prefrences_id = models.IntegerField(primary_key=True, null=False)
-    recommendation_id = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
-    prefrences_id = models.ForeignKey(Investment_Prefrences, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "investment_preference_to_recommendation_rel"
+        db_table = "stock_investment_prefrences"
